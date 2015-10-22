a4acc9f8ce870b904490877f0a82ae427146d4a8
Edward A Maxedon Sr <edadma@gmail.com>
2015-10-22 19:20:38 -0400

    fix bind fail problem

---
+++ src/main/scala/Main.scala
@@ -0,0 +1,25 @@
+package xyz.hyperreal.spraytemplate
+
+import akka.actor.{ActorSystem, Props}
+import akka.io.IO
+import spray.can.Http
+import akka.pattern.ask
+import akka.util.Timeout
+import scala.concurrent.duration._
+
+object Main extends App {
+
+  // we need an ActorSystem to host our application in
+  implicit val system = ActorSystem("on-spray-can")
+	implicit val context = system.dispatcher
+	
+  // create and start our service actor
+  val service = system.actorOf(Props[MyServiceActor], "demo-service")
+
+  implicit val timeout = Timeout(5.seconds)
+  // start a new HTTP server on port 8080 with our service actor as the handler
+  (IO(Http) ? Http.Bind(service, interface = "127.0.0.1", port = 8080)).map {
+		case _: Http.CommandFailed =>
+			system.shutdown
+		}
+}
